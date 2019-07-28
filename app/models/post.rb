8e606c949eebd352b74c6091f80c51d5567979cf
keisuke713 <nebakei.tkb713@gmail.com>
2019-07-28 19:16:36 +0900

    Add posts in user page

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: { maximum: 140 }
   validates :user_id, presence: true
+  validates :image, presence: true
 
   mount_uploader :image, ImageUploader
+
+  def text_slice
+    content.length > 50 ? "#{content.slice(0..19)}..." : content
+  end
 end
--- db/migrate/20190725212246_change_column_to_post_image.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToPostImage < ActiveRecord::Migration[5.2]
-  def change
-    change_column :posts, :image, :string, null: true
-  end
-end
