ba9e23a7d81faf0ddc86ed7821614678186c2d02
keisuke713 <nebakei.tkb713@gmail.com>
2019-07-31 21:42:05 +0900

    Remove some dead code

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,6 @@ require 'rspec/rails'
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
-# ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
@@ -61,7 +60,6 @@ RSpec.configure do |config|
   # config.extend ControllerMacros, type: :controller
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include SessionsHelper
-  # config.include SpecTestHelper, type: feature
   [:controller, :view, :request].each do |type|
     config.include ::Rails::Controller::Testing::TestProcess, type: type
     config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
