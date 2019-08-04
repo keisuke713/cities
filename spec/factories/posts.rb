892b53c5c1aac2a3c381854009ace8a31c796076
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-04 14:52:41 +0900

    done validation test of comment

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post, class: Post do
     content 'a' * 140
-    image nil
+    image Rack::Test::UploadedFile.new(File.join(Rails.root, 'app/assets/images/tokyo.jpg'))
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,8 +5,12 @@ RSpec.describe Comment, type: :model do
     FactoryBot.create(:user)
   }
 
+  let(:post_attributes) {
+    FactoryBot.attributes_for(:post)
+  }
+
   let(:post) {
-    FactoryBot.creat(:post, user_id: user.id)
+    user.posts.create(post_attributes)
   }
 
   let(:valid_attributes) {
@@ -14,20 +18,27 @@ RSpec.describe Comment, type: :model do
   }
 
   let(:comment) {
-    user.comments.build(:valid_attributes, post_id: post.id)
+    user.comments.build(valid_attributes)
   }
 
   it 'is valid with content' do
+    comment.post = post
     expect(comment).to be_valid
   end
 
   it 'is invalid without content' do
+    comment.post = post
     comment.content = nil
     expect(comment).to be_invalid
   end
 
   it 'is invalid with content too long' do
+    comment.post = post
     comment.content = 'a' * 141
+    expect(comment).to be_invalid
+  end
+
+  it 'is invalid with post_id' do
     expect(comment).to be_invalid
   end
 end
