bef3b32633cd8ffb27e4a65705603cd861297d3a
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-04 14:17:06 +0900

    Add test code of comment model

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :comment do
-    content "MyText"
+    content 'a' * 140
     user nil
     post nil
   end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {
+    FactoryBot.create(:user)
+  }
+
+  let(:post) {
+    FactoryBot.creat(:post, user_id: user.id)
+  }
+
+  let(:valid_attributes) {
+    FactoryBot.attributes_for(:comment)
+  }
+
+  let(:comment) {
+    user.comments.build(:valid_attributes, post_id: post.id)
+  }
+
+  it 'is valid with content' do
+    expect(comment).to be_valid
+  end
+
+  it 'is invalid without content' do
+    comment.content = nil
+    expect(comment).to be_invalid
+  end
+
+  it 'is invalid with content too long' do
+    comment.content = 'a' * 141
+    expect(comment).to be_invalid
+  end
 end
