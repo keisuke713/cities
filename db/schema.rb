60386012cbe97b1b1b2a2227202f17108c059fcf
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-16 06:52:48 +0900

    Add self-reference

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_04_043023) do
+ActiveRecord::Schema.define(version: 2019_08_15_211502) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_08_04_043023) do
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "comment_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
@@ -25,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_08_04_043023) do
   create_table "posts", force: :cascade do |t|
     t.text "content", null: false
     t.integer "user_id", null: false
-    t.string "image", null: false
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
