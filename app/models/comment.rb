7c396e4112eb12ae4bf5feeeb25ef7716ce65ce3
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-17 10:51:19 +0900

    Change self-reference of comment to Reply

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  has_many :replies, class_name: 'Comment', foreign_key: 'comment_id', dependent: :destroy
-  belongs_to :original_comment, class_name: 'Comment', foreign_key: 'comment_id', optional: true
   validates :content, presence: true, length: { maximum: 140 }
 end
--- db/migrate/20190815211502_add_comment_id_to_comment.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentIdToComment < ActiveRecord::Migration[5.2]
-  def change
-    add_column :comments, :comment_id, :integer
-  end
-end
