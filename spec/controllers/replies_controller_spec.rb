5e0d4d071edefa5396b5531509e4060cbf5325fb
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-21 07:18:31 +0900

    Add controller test for reply

--- spec/controllers/replies_controller_spec.rb
+++ spec/controllers/replies_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe RepliesController, type: :controller do
     user.posts.create(FactoryBot.attributes_for(:post))
   }
 
-  let(:comment) {
+  let!(:comment) {
     comment = user.comments.build(FactoryBot.attributes_for(:comment))
     comment.post = user_post
     comment.save
@@ -26,17 +26,52 @@ RSpec.describe RepliesController, type: :controller do
     valid_attributes.merge(comment_id: comment.id)
   }
 
+  let(:comment_params) {
+    { comment_id: comment.id }
+  }
+
   describe "GET #new" do
     it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+      get :new, params: comment_params
+      expect(response.status).to eq 200
+    end
+
+    it "assigns comment" do
+      get :new, params: comment_params
+      expect(assigns(:comment)).to eq comment
+    end
+
+    it "render new" do
+      get :new, params: comment_params
+      expect(response).to render_template 'new'
     end
   end
 
   describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+    context 'parameter is reasonable' do
+      it "is registered" do
+        expect {
+          post :create, params: { user_id: user.id, comment_id: comment.id, reply: valid_attributes }
+        }.to change( Reply, :count ).by(1)
+      end
+
+      it "redirect comment page" do
+        post :create, params: { user_id: user.id, comment_id: comment.id, reply: valid_attributes }
+        expect(response).to redirect_to comment
+      end
+    end
+
+    context "parameter isn't reasonable" do
+      it "isn't registered" do
+        expect {
+          post :create, params: { user_id: user.id, comment_id: comment.id }
+        }.to change( Reply, :count )
+      end
+
+      it "render new page" do
+        post :create, params: { user_id: user.id, comment_id: comment.id }
+        expect(response).to render_template 'new'
+      end
     end
   end
 
