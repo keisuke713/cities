21dc9932d5bf691c848adfd5b234127a6c80f8ff
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-23 07:33:52 +0900

    Use tap_method for replies_controller

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,4 +1,6 @@
 class RepliesController < ApplicationController
+  before_action :logged_in_user
+
   def new
     @comment = Comment.find(params[:comment_id])
     @reply = Reply.new
@@ -6,9 +8,10 @@ class RepliesController < ApplicationController
 
   def create
     @comment = Comment.find(params[:comment_id])
-    @reply = current_user.replies.build(reply_params)
-    @reply.comment = @comment
-    
+    @reply = current_user.replies.build(reply_params) do |reply|
+      reply.comment = @comment
+    end
+
     if @reply.save
       flash[:success] = 'success in replying'
       redirect_to comment_url(params[:comment_id])
