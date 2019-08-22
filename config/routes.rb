8c815fbbe2f9073f7bc342127ea8e098de704bec
keisuke713 <nebakei.tkb713@gmail.com>
2019-08-23 07:37:07 +0900

    Remove dead code

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
-Rails.application.routes.draw do
-  get 'replies/index'
-  get 'replies/new'
-  get 'replies/create'
+Rails.application.routes.draw do  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_page#home'
   get '/signup', to:'users#new'
