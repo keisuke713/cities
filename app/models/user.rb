27bfc4cbf359a6be1de9bef627f8f631381b89c9
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-07 10:28:12 +0900

    Generate relationships_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: :Relationship,
                    foreign_key: :followed_id,
                    dependent: :destroy
-  has_many :follower, through: :passive_relationships
+  has_many :followers, through: :passive_relationships
 
   before_save { self.email = email.downcase }
 
