f81877d82aad67c3fd4d7bc5028b2dc43ba034ea
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-11 07:21:48 +0900

    remove unnecessary code

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,8 +3,4 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: :User
   validates :follower_id, presence: true
   validates :followed_id, presence: true
-
-  def self.check(user, other_user)
-    Relationship.exists?(follower_id: user.id, followed_id: other_user.id)
-  end
 end
