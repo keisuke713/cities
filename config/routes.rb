bd5bce2fee2616cf3a6605d0b8255eef03c82d53
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-13 06:12:22 +0900

    Remove controller

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,11 @@ Rails.application.routes.draw do
     resources :posts, only: [:index, :new, :create]
     resources :book_marks, only: :index
     resources :relationships, only: [:create, :destroy]
-    member do
-      get :following, :followers
-    end
+    # member do
+    #   get :following, :followers
+    # end
+    resources :followings, only: :index, module: :users
+    resources :followers, only: :index, module: :users
   end
   resources :posts, only: :show do
     resources :comments, only: [:new, :create]
