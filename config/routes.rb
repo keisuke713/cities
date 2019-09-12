0b76a661d19ac98465db633e772cb9f4e988d361
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-13 06:15:00 +0900

    Generate Following Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :users do
+    get 'followings/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_page#home'
   get '/signup', to:'users#new'
