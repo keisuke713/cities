49d9d32b5905f91d12c53b450f1673ca636e7b1c
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-13 07:20:34 +0900

    Add test for followers#index

--- spec/controllers/users/followers_controller_spec.rb
+++ spec/controllers/users/followers_controller_spec.rb
@@ -3,9 +3,34 @@ require 'rails_helper'
 RSpec.describe Users::FollowersController, type: :controller do
 
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    let(:relationship) {
+      FactoryBot.create(:relationship)
+    }
+    let(:user) {
+      relationship.followed
+    }
+    let(:following_user) {
+      relationship.follower
+    }
+    let(:user_params) {
+      { user_id: user.id }
+    }
+    before do
+      log_in user
+    end
+    it "return http success" do
+      get :index, params: user_params
+      expect(response.status).to eq 200
+    end
+
+    it "assign followed user" do
+      get :index, params: user_params
+      expect(assigns(:users)).to include following_user
+    end
+
+    it "render index page" do
+      get :index, params: user_params
+      expect(response).to render_template 'index'
     end
   end
 
