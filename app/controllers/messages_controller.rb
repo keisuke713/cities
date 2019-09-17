3fc9654da57ccad1ec878b047dc03482c20509a8
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-18 07:46:14 +0900

    Make it possible to send messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,9 +6,25 @@ class MessagesController < ApplicationController
   end
 
   def create
+    message = current_user.send_messages.build(message_params)
+
+    if message.save
+      flash[:success] = 'succeeded in sending message'
+      redirect_to user_message_path(current_user.id, message_params[:receiver_id])
+    else
+      flash[:danger] = 'failed  to send message'
+      render 'show'
+    end
   end
 
   def show
+    @message = Message.new
     @messages = Message.all_messages(current_user.id, params[:id])
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :receiver_id)
   end
 end
