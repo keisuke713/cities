fc178777499d359bbdbef223d7f840ebb2c8d117
keisuke713 <nebakei.tkb713@gmail.com>
2019-09-29 14:16:46 +0900

    Add test for search_posts_controller

--- spec/controllers/posts/search_posts_controller_spec.rb
+++ spec/controllers/posts/search_posts_controller_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Posts::SearchPostsController, type: :controller do
+  let(:current_user) {
+    FactoryBot.create(:admin_user)
+  }
 
+  before do
+    log_in current_user
+  end
+
+  describe "GET #index" do
+    let!(:post) {
+      FactoryBot.create(:post)
+    }
+
+    let!(:post2) {
+      FactoryBot.create(:child_post)
+    }
+
+    let!(:params) {
+      { content: :aaa }
+    }
+
+    it 'returns http success' do
+      get :index, params: params
+      expect(:response).to eq 200
+    end
+
+    it 'assigns posts' do
+      get :index, params: params
+      expect(assigns(:posts)).to contain_exactly post
+    end
+
+    it 'render posts/index' do
+      get :index, params: params
+      expect(:response).to render_template 'posts/index'
+    end
+  end
 end
