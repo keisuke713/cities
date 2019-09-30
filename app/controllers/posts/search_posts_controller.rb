41ec70a91820285592491d230050e54394c184e4
keisuke713 <nebakei.tkb713@gmail.com>
2019-10-01 07:52:18 +0900

    split search_posts_controller#index to make it easy to maintain'

--- app/controllers/posts/search_posts_controller.rb
+++ app/controllers/posts/search_posts_controller.rb
@@ -1,17 +1,38 @@
 class Posts::SearchPostsController < ApplicationController
   def index
-    @posts =
-      if params[:keyword].empty?
-        Post.all
-      else
-        if params[:search_method] == 'user_match'
-          user_ids = User.match(params[:keyword]).pluck(:id)
-          Post.user_match(user_ids).includes(:user)
-        else
-          keyword = Search.condition(params[:search_method], params[:keyword])
-          Post.content_match(keyword).includes(:user)
-        end
-      end
+    @posts = fetch_posts(params[:keyword], params[:search_method])
     render 'posts/index'
+  end
+
+  private
+
+  def fetch_posts(keyword, search_method)
+    if keyword.empty?
+      fetch_all_posts
+    else
+      fetch_postss_by(keyword, search_method)
+    end
+  end
+
+  def fetch_all_posts
+    Post.all.includes(:user)
+  end
+
+  def fetch_postss_by(keyword, search_method)
+    if search_method == 'user_match'
+      fetch_posts_by_user_name(keyword)
+    else
+      fetch_posts_by_content(keyword, search_method)
+    end
+  end
+
+  def fetch_posts_by_user_name(keyword)
+    user_ids = User.match(keyword).pluck(:id)
+    Post.user_match(user_ids).includes(:user)
+  end
+
+  def fetch_posts_by_content(keyword, search_method)
+    keyword = Search.condition(search_method, keyword)
+    Post.content_match(keyword).includes(:user)
   end
 end
