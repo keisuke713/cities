4bf8113edba85f0e50db99c94f0bde7eaea3f29a
keisuke713 <nebakei.tkb713@gmail.com>
2019-10-02 06:14:38 +0900

    split search_users_controller#index to make it easy to maintain

--- app/controllers/users/search_users_controller.rb
+++ app/controllers/users/search_users_controller.rb
@@ -1,16 +1,37 @@
 class Users::SearchUsersController < ApplicationController
   def index
-    @users =
-      if params[:name].empty?
-        User.all
-      else
-        unless params[:search_method] === 'exclude'
-          search_condition = Search.condition(params[:search_method], params[:name])
-          User.match(search_condition)
-        else
-          User.exclude(params[:name])
-        end
-      end
+    @users = fetch_users(params[:search_method], params[:name])
     render 'users/index'
+  end
+
+  private
+
+  def fetch_users(search_method, name)
+    if name.empty?
+      fetch_all_users
+    else
+      fetch_users_by(search_method, name)
+    end
+  end
+
+  def fetch_all_users
+    User.all
+  end
+
+  def fetch_users_by(search_method, name)
+    unless search_method === 'exclude'
+      fetch_users_by_user_name(search_method, name)
+    else
+      fetch_users_by_excluding(name)
+    end
+  end
+
+  def fetch_users_by_user_name(search_method, name)
+    search_condition = Search.condition(search_method, name)
+    User.match(search_condition)
+  end
+
+  def fetch_users_by_excluding(name)
+    User.exclude(name)
   end
 end
