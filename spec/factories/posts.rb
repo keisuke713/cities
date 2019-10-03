cc1775a1d14ae5228de0a2bec66bb1f7f20f9606
keisuke713 <nebakei.tkb713@gmail.com>
2019-10-04 07:52:39 +0900

    Add test for PostsController#new

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -25,4 +25,11 @@ FactoryBot.define do
     association :user, factory: :user2
     association :parent_post, factory: :post
   end
+
+  factory :draft, class: Post do
+    content 'a' * 140
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/tokyo.jpg'), 'image/jpg') }
+    user
+    initialize_with { Post.find_or_create_by(content: content, user: user)}
+  end
 end
