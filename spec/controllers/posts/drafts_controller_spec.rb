ef6a463f95622d2aa8cdf9679ee597c4c9c096a4
keisuke713 <nebakei.tkb713@gmail.com>
2019-10-05 09:46:38 +0900

    Add test for posts/drafts#idnex

--- spec/controllers/posts/drafts_controller_spec.rb
+++ spec/controllers/posts/drafts_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Posts::DraftsController, type: :controller do
+  let(:user) {
+    FactoryBot.create(:user)
+  }
+  before do
+    log_in user
+  end
 
+  describe "GET #index" do
+    let(:draft) {
+      FactoryBot.create(:draft)
+    }
+    let(:post) {
+      FactoryBot.create(:post)
+    }
+    it "returns http success" do
+      get :index
+      expect(response.status).to eq 200
+    end
+
+    it "assigns drafts" do
+      get :index
+      expect(assigns(drats)).to contain_exactly draft
+    end
+
+    it "render index page" do
+      get :index
+      expect(response).to render_template 'index'
+    end
+  end
 end
